be161a3c8ca4e6e5888eac527bc628d448626f5f
Muskan Gupta <muskaanguppta@gmail.com>
2024-07-07 15:24:46 +0530

    Create 1518.WaterBottles.java

---
+++ 1518.WaterBottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int empty=numBottles;
+        int ans=numBottles;
+        while(empty>=numExchange){
+            int ad=empty/numExchange;
+            ans=ans+ad;
+            int lef=ad+empty-ad*numExchange;
+            empty=lef;
+        }
+        return ans;
+    }
+}
