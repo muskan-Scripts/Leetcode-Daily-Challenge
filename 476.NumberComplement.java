61781d21d4ce48a1fd45cbcb3b485f1e8c81d846
Muskan Gupta <muskaanguppta@gmail.com>
2024-08-22 19:57:28 +0530

    Update 476.NumberComplement.java

--- 476.NumberComplement.java
+++ 476.NumberComplement.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int findComplement(int num) {
+       StringBuilder sb= new StringBuilder();
+       while(num>0){
+        int res= num%2;
+        num=num/2;
+        if(res==1)sb.append('0');
+        else{sb.append('1');}
 
+
+       }
+       sb.reverse();
+       if(sb.length()==0)return 1;
+       int result=0;
+       String curr= sb.toString();
+       for(int i=0; i<curr.length(); i++){
+        if(curr.charAt(i)=='1'){
+            result=result+(int)(Math.pow(2, curr.length()-i-1));
+        }
+       }
+
+       return result;
+    }
+}
