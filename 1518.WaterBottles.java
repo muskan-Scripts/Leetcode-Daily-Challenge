11a18b1c7f0b6d537f8a8d12c9b4d20dece7907a
Muskan Gupta <muskaanguppta@gmail.com>
2024-11-18 23:39:08 +0530

    Update 1518.WaterBottles.java

--- 1518.WaterBottles.java
+++ 1518.WaterBottles.java
@@ -2,6 +2,7 @@ class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int empty=numBottles;
         int ans=numBottles;
+        
         while(empty>=numExchange){
             int ad=empty/numExchange;
             ans=ans+ad;
