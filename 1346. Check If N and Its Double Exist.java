6a7265458f7062e280e3639a31541cc6d347960b
Muskan Gupta <muskaanguppta@gmail.com>
2024-12-01 23:45:04 +0530

    Create 1346. Check If N and Its Double Exist.java

---
+++ 1346. Check If N and Its Double Exist.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            int target = 2 * arr[i];
+            int lo = 0, hi = arr.length - 1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (arr[mid] == target && mid != i) 
+                    return true;
+                if (arr[mid] < target) 
+                    lo = mid + 1;
+                else 
+                    hi = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
+
+// TC: O(n * logn), SC: O(1)
